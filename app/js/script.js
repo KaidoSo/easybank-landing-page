92ac3ef1cf1139e43a2e2e55e3684796c210afdf
KaidoSo <kaido.soomlais@gmail.com>
2021-02-19 13:26:03 +0200

    added fade in and out animation to overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,17 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 btnHamburger.addEventListener('click', function(){
     console.log('click hamburger');
 
-    if(header.classList.contains('open')){
+    if(header.classList.contains('open')){ // Close Hamburger menu
         header.classList.remove('open');
-    } else {
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    } else { // Open Hamburger menu
         header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
     }
 });
