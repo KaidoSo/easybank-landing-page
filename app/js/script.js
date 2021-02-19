3493974366a6a87ddc353d5807f9918046d989d2
KaidoSo <kaido.soomlais@gmail.com>
2021-02-19 11:10:10 +0200

    animated the hamburger button

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,11 @@
-console.log('testing');
+const btnHamburger = document.querySelector('#btnHamburger');
+
+btnHamburger.addEventListener('click', function(){
+    console.log('open hamburger')
+
+    if(btnHamburger.classList.contains('open')){
+        btnHamburger.classList.remove('open');
+    } else {
+        btnHamburger.classList.add('open');
+    }
+});
